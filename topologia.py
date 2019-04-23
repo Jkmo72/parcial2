3b0db36bcb096cdc46fc44828429ffd9754d2917
Jkmo72 <47998782+Jkmo72@users.noreply.github.com>
2019-04-22 20:47:02 -0500

    Create topologia.py

---
+++ topologia.py
@@ -0,0 +1,23 @@
+# Codigo parcial 2 electica 4 Camilo Bohada - Camilo Jerez
+# Unitec
+
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+
+    def __init__( self ):
+              
+      Topo.__init__( self )
+
+        # Add hosts and switches
+      Host1 = self.addHost( 'h1' )
+      Host2 = self.addHost( 'h2' )
+      Switch1 = self.addSwitch( 's1' )
+  
+
+        # Add links
+      self.addLink( Host1, Switch1 )
+      self.addLink( Host2, Switch1 )
+ 
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
